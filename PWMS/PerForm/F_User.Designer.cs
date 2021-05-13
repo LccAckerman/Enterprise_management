2e37cfe70227b6ace7beaccc9c50c982a1d2d483
yuanyuanyuanheng <46164728+yuanyuanyuanheng@users.noreply.github.com>
2021-05-13 14:49:04 +0800

    Update F_User.Designer.cs

--- PWMS/PerForm/F_User.Designer.cs
+++ PWMS/PerForm/F_User.Designer.cs
@@ -2,7 +2,15 @@ namespace PWMS.PerForm
 {
     partial class F_User
     {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
 
         protected override void Dispose(bool disposing)
         {
