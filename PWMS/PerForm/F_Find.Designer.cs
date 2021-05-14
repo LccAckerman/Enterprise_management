26c12226f81022c73cf897c703daf50f3e35478d
Vilhana <w.vilnana@gmail.com>
2021-05-14 09:38:22 +0800

    Update F_Find.Designer.cs
    提交框架 （未完成所有）

--- PWMS/PerForm/F_Find.Designer.cs
+++ PWMS/PerForm/F_Find.Designer.cs
@@ -1,1 +1,475 @@
-﻿
+namespace PWMS.PerForm
+{
+    partial class F_Find
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Find_Laborage = new System.Windows.Forms.ComboBox();
+            this.Find_Kultur = new System.Windows.Forms.ComboBox();
+            this.Find_Business = new System.Windows.Forms.ComboBox();
+            this.Find_Branch = new System.Windows.Forms.ComboBox();
+            this.Find_Employee = new System.Windows.Forms.ComboBox();
+            this.Find_Duthcall = new System.Windows.Forms.ComboBox();
+            this.Find_Visage = new System.Windows.Forms.ComboBox();
+            this.Find_Folk = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Find_Speciality = new System.Windows.Forms.ComboBox();
+            this.Find_School = new System.Windows.Forms.ComboBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.Find2_WorkDate = new System.Windows.Forms.MaskedTextBox();
+            this.Find1_WorkDate = new System.Windows.Forms.MaskedTextBox();
+            this.Find_Pact_Y = new System.Windows.Forms.TextBox();
+            this.Pact_Y_Sign = new System.Windows.Forms.ComboBox();
+            this.Find_M_Pay = new System.Windows.Forms.TextBox();
+            this.M_Pay_Sign = new System.Windows.Forms.ComboBox();
+            this.Find_City = new System.Windows.Forms.ComboBox();
+            this.Find_BeAware = new System.Windows.Forms.ComboBox();
+            this.Find_WorkLength = new System.Windows.Forms.TextBox();
+            this.WorkLength_Sign = new System.Windows.Forms.ComboBox();
+            this.Find_Age = new System.Windows.Forms.TextBox();
+            this.Find_Marriage = new System.Windows.Forms.ComboBox();
+            this.Age_Sign = new System.Windows.Forms.ComboBox();
+            this.Find_Sex = new System.Windows.Forms.ComboBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Find_Laborage);
+            this.groupBox1.Controls.Add(this.Find_Kultur);
+            this.groupBox1.Controls.Add(this.Find_Business);
+            this.groupBox1.Controls.Add(this.Find_Branch);
+            this.groupBox1.Controls.Add(this.Find_Employee);
+            this.groupBox1.Controls.Add(this.Find_Duthcall);
+            this.groupBox1.Controls.Add(this.Find_Visage);
+            this.groupBox1.Controls.Add(this.Find_Folk);
+            this.groupBox1.Location = new System.Drawing.Point(8, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "基本信息";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(308, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 12);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "职称类别：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "部门类别：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(324, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "工资类别：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "职务类别：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "职工类别：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "政治面貌：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "文化程度：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "民族类别：";
+            // 
+            // Find_Laborage
+            // 
+            this.Find_Laborage.FormattingEnabled = true;
+            this.Find_Laborage.Location = new System.Drawing.Point(390, 51);
+            this.Find_Laborage.Name = "Find_Laborage";
+            this.Find_Laborage.Size = new System.Drawing.Size(87, 20);
+            this.Find_Laborage.TabIndex = 8;
+            // 
+            // Find_Kultur
+            // 
+            this.Find_Kultur.FormattingEnabled = true;
+            this.Find_Kultur.Location = new System.Drawing.Point(231, 23);
+            this.Find_Kultur.Name = "Find_Kultur";
+            this.Find_Kultur.Size = new System.Drawing.Size(87, 20);
+            this.Find_Kultur.TabIndex = 7;
+            // 
+            // Find_Business
+            // 
+            this.Find_Business.FormattingEnabled = true;
+            this.Find_Business.Location = new System.Drawing.Point(231, 51);
+            this.Find_Business.Name = "Find_Business";
+            this.Find_Business.Size = new System.Drawing.Size(87, 20);
+            this.Find_Business.TabIndex = 6;
+            // 
+            // Find_Branch
+            // 
+            this.Find_Branch.FormattingEnabled = true;
+            this.Find_Branch.Location = new System.Drawing.Point(72, 77);
+            this.Find_Branch.Name = "Find_Branch";
+            this.Find_Branch.Size = new System.Drawing.Size(87, 20);
+            this.Find_Branch.TabIndex = 4;
+            // 
+            // Find_Employee
+            // 
+            this.Find_Employee.FormattingEnabled = true;
+            this.Find_Employee.Location = new System.Drawing.Point(72, 51);
+            this.Find_Employee.Name = "Find_Employee";
+            this.Find_Employee.Size = new System.Drawing.Size(87, 20);
+            this.Find_Employee.TabIndex = 3;
+            // 
+            // Find_Duthcall
+            // 
+            this.Find_Duthcall.FormattingEnabled = true;
+            this.Find_Duthcall.Location = new System.Drawing.Point(390, 77);
+            this.Find_Duthcall.Name = "Find_Duthcall";
+            this.Find_Duthcall.Size = new System.Drawing.Size(87, 20);
+            this.Find_Duthcall.TabIndex = 2;
+            // 
+            // Find_Visage
+            // 
+            this.Find_Visage.FormattingEnabled = true;
+            this.Find_Visage.Location = new System.Drawing.Point(390, 23);
+            this.Find_Visage.Name = "Find_Visage";
+            this.Find_Visage.Size = new System.Drawing.Size(87, 20);
+            this.Find_Visage.TabIndex = 1;
+            // 
+            // Find_Folk
+            // 
+            this.Find_Folk.FormattingEnabled = true;
+            this.Find_Folk.Location = new System.Drawing.Point(72, 23);
+            this.Find_Folk.Name = "Find_Folk";
+            this.Find_Folk.Size = new System.Drawing.Size(87, 20);
+            this.Find_Folk.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+          
+            // 
+            // Find_Speciality
+            // 
+           
+            // 
+            // Find_School
+            // 
+         
+            // 
+            // label21
+            // 
+          
+            // 
+            // label20
+            // 
+           
+            // 
+            // Find2_WorkDate
+          
+            // 
+            // Find1_WorkDate
+            // 
+           
+            // 
+            // Find_Pact_Y
+            // 
+           
+            // 
+            // Pact_Y_Sign
+            // 
+          
+            // 
+            // Find_M_Pay
+            // 
+          
+            // 
+            // M_Pay_Sign
+            // 
+           
+            // 
+            // Find_City
+            // 
+           
+            // 
+            // Find_BeAware
+            // 
+           
+            // 
+            // Find_WorkLength
+            // 
+         
+            // 
+            // WorkLength_Sign
+            // 
+           
+            // 
+            // Find_Age
+            // 
+          
+            // Find_Marriage
+            // 
+            
+            // 
+            // Age_Sign
+            // 
+          
+            // 
+            // Find_Sex
+            // 
+           
+            // 
+            // label19
+            // 
+           
+            // 
+            // label18
+            // 
+           
+            // 
+            // label17
+            // 
+          
+            // 
+            // label16
+            // 
+           
+            // 
+            // label15
+            // 
+           
+            // 
+            // label14
+            // 
+           
+            // 
+            // label13
+            // 
+         
+            // 
+            // label12
+            // 
+           
+            // 
+            // label11
+            // 
+            
+            // 
+            // label10
+            // 
+           
+            // 
+            // label9
+            // 
+           
+            // 
+            // groupBox3
+            // 
+           
+            // 
+            // checkBox1
+            // 
+           
+            // 
+            // button3
+            // 
+          
+            // 
+            // button2
+            // 
+          
+            // 
+            // button1
+            // 
+         
+            // 
+            // radioButton2
+            // 
+           
+            // 
+            // radioButton1
+            // 
+           
+            // 
+            // groupBox4
+            // 
+          
+            // 
+            // dataGridView1
+            // 
+        
+        
+            // 
+            // F_Find
+            // 
+           
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox Find_Laborage;
+        private System.Windows.Forms.ComboBox Find_Kultur;
+        private System.Windows.Forms.ComboBox Find_Business;
+        private System.Windows.Forms.ComboBox Find_Branch;
+        private System.Windows.Forms.ComboBox Find_Employee;
+        private System.Windows.Forms.ComboBox Find_Duthcall;
+        private System.Windows.Forms.ComboBox Find_Visage;
+        private System.Windows.Forms.ComboBox Find_Folk;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox Find_Age;
+        private System.Windows.Forms.ComboBox Find_Marriage;
+        private System.Windows.Forms.ComboBox Age_Sign;
+        private System.Windows.Forms.ComboBox Find_Sex;
+        private System.Windows.Forms.TextBox Find_WorkLength;
+        private System.Windows.Forms.ComboBox WorkLength_Sign;
+        private System.Windows.Forms.ComboBox Find_City;
+        private System.Windows.Forms.ComboBox Find_BeAware;
+        private System.Windows.Forms.TextBox Find_Pact_Y;
+        private System.Windows.Forms.ComboBox Pact_Y_Sign;
+        private System.Windows.Forms.TextBox Find_M_Pay;
+        private System.Windows.Forms.ComboBox M_Pay_Sign;
+        private System.Windows.Forms.MaskedTextBox Find2_WorkDate;
+        private System.Windows.Forms.MaskedTextBox Find1_WorkDate;
+        private System.Windows.Forms.ComboBox Find_Speciality;
+        private System.Windows.Forms.ComboBox Find_School;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
