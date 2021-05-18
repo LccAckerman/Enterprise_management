db6cc3204634a088acd0a5faa280f415faa67f6f
HJYAn <2454311648@qq.com>
2021-05-18 16:51:54 +0800

    519

--- PWMS/InfoAddForm/F_Address.cs
+++ PWMS/InfoAddForm/F_Address.cs
@@ -230,6 +230,7 @@ namespace PWMS.InfoAddForm
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 25);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // comboBox1
             // 
@@ -258,6 +259,11 @@ namespace PWMS.InfoAddForm
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
