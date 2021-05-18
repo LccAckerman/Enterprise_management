4af23b476bd63050206ba49b3c2a0315456019c6
HJYAn <2454311648@qq.com>
2021-05-18 08:28:02 +0800

    518

--- PWMS/InfoAddForm/F_Address.cs
+++ PWMS/InfoAddForm/F_Address.cs
@@ -42,19 +42,223 @@ namespace PWMS.InfoAddForm
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (this.Address_1.Text != "")
-            {
-                MyMC.Part_SaveClass("ID,Name,Sex,Phone,Handset,WordPhone,QQ,E_Mail", Address_ID, "", this.groupBox1.Controls, "Address_", "tb_AddressBook", 8, (int)this.Tag);
-                MyDataClass.getsqlcom(ModuleClass.MyModule.ADDs);
-                this.Close();
-            }
-            else
-                MessageBox.Show("人员姓名不能为空。");
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox2;
+        private Button Address_Quit;
+        private Button Address_Delete;
+        private Button Address_Amend;
+        private Button Address_Add;
+        private GroupBox groupBox1;
+        private Button button1;
+        private Label label2;
+        private Label label1;
+        private Button button5;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+
+        private void InitializeComponent()
+        {
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Address_Quit = new System.Windows.Forms.Button();
+            this.Address_Delete = new System.Windows.Forms.Button();
+            this.Address_Amend = new System.Windows.Forms.Button();
+            this.Address_Add = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(116, 116);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(603, 251);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "数据表";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 25);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(581, 215);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Address_Quit);
+            this.groupBox2.Controls.Add(this.Address_Delete);
+            this.groupBox2.Controls.Add(this.Address_Amend);
+            this.groupBox2.Controls.Add(this.Address_Add);
+            this.groupBox2.Location = new System.Drawing.Point(116, 374);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(603, 64);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // Address_Quit
+            // 
+            this.Address_Quit.Location = new System.Drawing.Point(447, 25);
+            this.Address_Quit.Margin = new System.Windows.Forms.Padding(4);
+            this.Address_Quit.Name = "Address_Quit";
+            this.Address_Quit.Size = new System.Drawing.Size(100, 29);
+            this.Address_Quit.TabIndex = 9;
+            this.Address_Quit.Text = "退出";
+            this.Address_Quit.UseVisualStyleBackColor = true;
+            // 
+            // Address_Delete
+            // 
+            this.Address_Delete.Location = new System.Drawing.Point(319, 25);
+            this.Address_Delete.Margin = new System.Windows.Forms.Padding(4);
+            this.Address_Delete.Name = "Address_Delete";
+            this.Address_Delete.Size = new System.Drawing.Size(100, 29);
+            this.Address_Delete.TabIndex = 8;
+            this.Address_Delete.Text = "删除";
+            this.Address_Delete.UseVisualStyleBackColor = true;
+            // 
+            // Address_Amend
+            // 
+            this.Address_Amend.Location = new System.Drawing.Point(185, 25);
+            this.Address_Amend.Margin = new System.Windows.Forms.Padding(4);
+            this.Address_Amend.Name = "Address_Amend";
+            this.Address_Amend.Size = new System.Drawing.Size(100, 29);
+            this.Address_Amend.TabIndex = 7;
+            this.Address_Amend.Text = "修改";
+            this.Address_Amend.UseVisualStyleBackColor = true;
+            // 
+            // Address_Add
+            // 
+            this.Address_Add.Location = new System.Drawing.Point(51, 25);
+            this.Address_Add.Margin = new System.Windows.Forms.Padding(4);
+            this.Address_Add.Name = "Address_Add";
+            this.Address_Add.Size = new System.Drawing.Size(100, 29);
+            this.Address_Add.TabIndex = 6;
+            this.Address_Add.Text = "添加";
+            this.Address_Add.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(116, 40);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(603, 68);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(529, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "全部";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "查询条件：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "查询类型：";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(461, 26);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 29);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "查询";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(317, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "姓名",
+            "性别",
+            "邮箱地址"});
+            this.comboBox1.Location = new System.Drawing.Point(100, 29);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // F_Address
+            // 
+            this.ClientSize = new System.Drawing.Size(834, 478);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "F_Address";
+            this.Text = "F_AddressList";
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
     }
 }
