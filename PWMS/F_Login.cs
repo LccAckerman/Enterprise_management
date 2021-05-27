26f52acfd21e8ad75b6629ba4c623e7f79d349e2
LccAckerman <lccackerman@mails.ccnu.edu.cn>
2021-05-27 17:33:18 +0800

    'WordPad'

--- PWMS/F_Login.cs
+++ PWMS/F_Login.cs
@@ -20,41 +20,12 @@ namespace PWMS
 
         private void butClose_Click(object sender, EventArgs e)
         {
-            if ((int)(this.Tag) == 1)
-            {
-                DataClass.MyMeans.Login_n = 3;
-                Application.Exit();
-            }
-            else
-                if ((int)(this.Tag) == 2)
-                    this.Close();
+
         }
 
         private void butLogin_Click(object sender, EventArgs e)
         {
-            if (textName.Text != "" & textPass.Text != "")
-            {
-                SqlDataReader temDR = MyClass.getcom("select * from tb_Login where Name='" + textName.Text.Trim() + "' and Pass='" + textPass.Text.Trim() + "'");
-                bool ifcom = temDR.Read();
-                if (ifcom)
-                {
-                    DataClass.MyMeans.Login_Name = textName.Text.Trim();
-                    DataClass.MyMeans.Login_ID = temDR.GetString(0);
-                    DataClass.MyMeans.My_con.Close();
-                    DataClass.MyMeans.My_con.Dispose();
-                    DataClass.MyMeans.Login_n = (int)(this.Tag);
-                    this.Close();
-                }
-                else
-                {
-                    MessageBox.Show("用户名或密码错误！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    textName.Text = "";
-                    textPass.Text = "";
-                }
-                MyClass.con_close();
-            }
-            else
-                MessageBox.Show("请将登录信息添写完整！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
 
         private void F_Login_Load(object sender, EventArgs e)
@@ -76,14 +47,12 @@ namespace PWMS
 
         private void textName_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == '\r')
-                textPass.Focus();
+
         }
 
         private void textPass_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == '\r')
-                butLogin.Focus();
+
         }
 
         private void F_Login_Activated(object sender, EventArgs e)
@@ -91,5 +60,117 @@ namespace PWMS
             textName.Focus();
         }
 
+        private Label label2;
+        private Label label1;
+        private TextBox textPass;
+        private TextBox textName;
+        private Button butClose;
+        private Button butLogin;
+        private PictureBox pictureBox1;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Login));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.butClose = new System.Windows.Forms.Button();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(89, 181);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "密  码：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(89, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "用户名：";
+            // 
+            // textPass
+            // 
+            this.textPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textPass.Location = new System.Drawing.Point(188, 175);
+            this.textPass.Margin = new System.Windows.Forms.Padding(4);
+            this.textPass.Name = "textPass";
+            this.textPass.PasswordChar = '*';
+            this.textPass.Size = new System.Drawing.Size(158, 25);
+            this.textPass.TabIndex = 17;
+            this.textPass.Text = "111";
+            // 
+            // textName
+            // 
+            this.textName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textName.Location = new System.Drawing.Point(188, 141);
+            this.textName.Margin = new System.Windows.Forms.Padding(4);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(158, 25);
+            this.textName.TabIndex = 16;
+            this.textName.Text = "TSoft";
+            // 
+            // butClose
+            // 
+            this.butClose.Location = new System.Drawing.Point(255, 219);
+            this.butClose.Margin = new System.Windows.Forms.Padding(4);
+            this.butClose.Name = "butClose";
+            this.butClose.Size = new System.Drawing.Size(69, 29);
+            this.butClose.TabIndex = 15;
+            this.butClose.Text = "取消";
+            this.butClose.UseVisualStyleBackColor = true;
+            // 
+            // butLogin
+            // 
+            this.butLogin.Location = new System.Drawing.Point(113, 219);
+            this.butLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(71, 29);
+            this.butLogin.TabIndex = 14;
+            this.butLogin.Text = "登录";
+            this.butLogin.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // F_Login
+            // 
+            this.ClientSize = new System.Drawing.Size(470, 268);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.butClose);
+            this.Controls.Add(this.butLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "F_Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
     }
 }
